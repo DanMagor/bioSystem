aac866d869ce5932676db7b79e875e7753cf1a3d
Anton Skudarnov <Anton Skudarnov>
2017-06-05 20:33:14 +0600

    init move logic

--- BioSystem/Assets/Scripts/herbivoreBehavior.cs
+++ BioSystem/Assets/Scripts/herbivoreBehavior.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class herbivoreBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+public class herbivoreBehavior : MonoBehaviour {
+    Rigidbody2D m_Rigidbody2D;
+    // Use this for initialization
+    bool m_move = true;
+    float m_speed = 1f;
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (checkEvent())
+        {
+        }
+        else {
+            if(m_move)
+            moveToPoint();
+        }
+
 	}
+
+
+    void moveToPoint()
+    {
+        
+        GameObject[] points = GameObject.FindGameObjectsWithTag("point");
+        GameObject target = points[0];
+        float t_x = target.transform.position.x;
+        float t_y = target.transform.position.y;
+        float dir_x, dir_y;
+        if (t_x < m_Rigidbody2D.transform.position.x) dir_x = -1; else dir_x = 1;
+        if (t_y < m_Rigidbody2D.transform.position.y) dir_y = -1; else dir_y = 1;
+        m_Rigidbody2D.velocity = new Vector2(m_speed * dir_x, m_speed * dir_y);
+        
+       
+       
+    }
+
+    void move() {
+
+    } 
+    bool checkEvent() {
+        return false; 
+    }
 }
+
